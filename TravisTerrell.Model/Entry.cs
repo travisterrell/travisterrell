72924fc498a98809a3393831447d37882ae977dc
Travis Terrell <travis@travisterrell.com>
2016-11-25 19:13:54 -0600

    Add model classes for blog.

---
+++ TravisTerrell.Model/Entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravisTerrell.Model
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public Author Author { get; set; }
+        
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
